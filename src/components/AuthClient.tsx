b3ae7cfaec79d5aff4343458428075051ed04cc9
kyoungjoopark <kyoungjoopark2@gmail.com>
2025-07-17 10:00:32 +0900

    fix: Suspense boundary 추가 및 SSR 호환성 개선

--- src/components/AuthClient.tsx
+++ src/components/AuthClient.tsx
@@ -20,7 +20,7 @@ export default function AuthClient() {
       email,
       password,
       options: {
-        emailRedirectTo: `${location.origin}/auth/callback`,
+        emailRedirectTo: `${window.location.origin}/auth/callback`,
       },
     })
     if (error) {
